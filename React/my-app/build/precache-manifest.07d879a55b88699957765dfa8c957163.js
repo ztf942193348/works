941cb0df159620eb56332c95a9f8b08cd88e9615
ztf <942193348@qq.com>
2019-08-12 22:40:08 +0800

    改了一下打包后的路径

---
+++ React/my-app/build/precache-manifest.07d879a55b88699957765dfa8c957163.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7598355a95ca738b5d7290aea639566",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0fbabeb93e68157f6ae2",
+    "url": "./static/js/2.a541aa0c.chunk.js"
+  },
+  {
+    "revision": "9b157760e37a209c5f04",
+    "url": "./static/js/main.01c8c9ed.chunk.js"
+  },
+  {
+    "revision": "92b7010270b0ac324f72",
+    "url": "./static/js/runtime~main.dd97c86a.js"
+  }
+]);
